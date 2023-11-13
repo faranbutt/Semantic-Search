f956125b05afc9ed7d7dfb0f784147cbb8a72a1d
Faran Butt <66089079+faranbutt@users.noreply.github.com>
2023-11-13 19:01:51 +0000

    Updating Search page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link"
 export default function Search(){
     return(
-        <div>
-            Search Page under development......
+        <div className="w-screen pr-7">
+            <div className='flex justify-end m-10'>
+                <div className='bg-blue-500 text-sm rounded-xl text-white px-4 py-2'>
+                    <Link href={'/'}>Upload Document</Link>
+                </div>
+            </div>
+            <div>
+                Search Document
+            </div>
         </div>
     )
 }
