2157d4fd9fd75620b974bc71cd78dd05b9fca355
Faran Butt <66089079+faranbutt@users.noreply.github.com>
2023-11-15 15:39:06 +0000

    Added Endpoints

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider, EmotionCache } from "@emotion/react";
 import theme from "@/lib/theme";
 import createEmotionCache from "@/lib/createEmotionCache";
-
+import {client} from '@gradio/client';
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,32 @@
+
 import Head from "next/head";
 import Link from "next/link";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import axios from "axios";
+import { client } from "@gradio/client";
 export default function Home() {
   const [file,setFile] = useState(null);
   const [result,setResult] = useState('');
+  const [query,setQuery] = useState('');
+  const [collection,setCollection] = useState('');
+  
   const handleFileChange = (event:any) => {
     console.log(event.target.files[0])
     setFile(event.target.files[0]);
   }
   const SendFile = async() => {
+    console.log("File Recieved",file)
+    console.log("Query Recieved",query)
+    console.log("Collection Recieved",collection)
     try{
-      const response = await axios.post('/embed',{
-        "collection_name": "{collection_name}",
-         "file_url": file
-      },{
-        headers:{
-          'Content-Type':"application/json"
-        }
-      })
+      //const app  = await client('https://teamtonic-herechatbackend.hf.space/--replicas/jfflh/',{hf_token:'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'});
+//       const result:any = await app.predict("/predict", [		
+// 				"Hello!!", // string  in 'Query' Textbox component
+// 				file, 	// blob in 'PDF File' File component		
+// 				"Hello!!", // string  in 'Collection Name' Textbox component
+// 	     ]);
+// console.log(result.data);
     }catch(error){
       console.error('Error:', error);
     }
@@ -46,12 +52,27 @@ export default function Home() {
         </div>
       
         <div className=''>
-          <div className="flex gap-2">
-            <div className="">
-              <Input type='file' onChange={handleFileChange}/>
-              <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">TXT, DOCX, PPTX, JPG, PNG, HTML or PDF</p>
+          <div className="">
+            <div className="flex flex-col justify-center items-center gap-4">
+              <div className="flex justify-center gap-4">
+              <div>
+                <label htmlFor="" className="font-bold text-3xl">Enter Query</label>
+                <Input type='text' onChange={(e)=>setQuery(e.target.value)}/>
+              </div>
+              <div>
+                <label htmlFor="" className="font-bold text-3xl">Enter Document</label>
+                <Input type='file' onChange={handleFileChange}/>
+                <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">TXT, DOCX, PPTX, JPG, PNG, HTML or PDF</p>
+              </div>
+              <div>
+                <label htmlFor="" className="font-bold text-3xl">Collection</label>
+                <Input type='text' onChange={(e) => setCollection(e.target.value)}/>
+              </div>
+              </div>
+              <div>
+                <Button className='border-2 border-blue-500 bg-white text-blue-500 font-bold hover:text-white hover:bg-blue-500' disabled={!file} onClick={SendFile} >Submit</Button>
+              </div>
             </div>
-            <Button className='border-2 border-blue-500 bg-white text-blue-500 font-bold hover:text-white hover:bg-blue-500' disabled={!file} onClick={SendFile} >Submit</Button>
           </div>
         </div>
       </div>
